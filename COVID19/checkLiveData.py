d0a7d6851915200eda325b02f5cf1f0362772cab
Sumukh Jadhav <48212565+SumukhJadhav@users.noreply.github.com>
2020-09-24 18:00:40 +0530

    Tweaks

--- COVID19/checkLiveData.py
+++ COVID19/checkLiveData.py
@@ -10,22 +10,22 @@ import sys
 import re
 
 
-mydb = mysql.connector.connect(host = "localhost", user = "smoke", passwd = "hellomoto", database = "test")
+mydb = mysql.connector.connect(host = "localhost", user = "smoke", passwd = "hellomoto", database = "test", autocommit=True)
 cursor = mydb.cursor()
-
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi("CheckLiveData.ui", self)
-        self.UpdataData.released.connect(lambda: getName())
-
-        def getName():
+        self.UpdataData.released.connect(lambda: newCases())
+        
+        def newCases():
+            
             locality = self.lineEdit.text().title()
-            self.lineEdit.clear()
+            self.lineEdit.clear()         
             print(locality)
-
+            
             try:
                 search(locality)
                 #print(Grecovered)
@@ -61,11 +61,6 @@ class Ui(QtWidgets.QMainWindow):
                     self.Deaths.setText("Deaths\n - - -")
 
 
-
-            
-
-
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
 
@@ -73,6 +68,7 @@ def main():
     window.show()
 
     app.exec_()
+
 
 def net_check():
     try:
@@ -82,70 +78,6 @@ def net_check():
         print("Net fail")
         return False
 
-
-def createDB():
-    
-    r = requests.get("https://api.covid19india.org/state_district_wise.json").json()
-
-    districts = []
-    for i in r.values():
-        for k in i['districtData'].keys():
-            districts.append(k)
-
-
-    confirmed_list = list([v['confirmed'] for i in r.values() for v in i['districtData'].values()])
-    active_list = list([v['active'] for i in r.values() for v in i['districtData'].values()])
-    recovered_list = list([v['recovered'] for i in r.values() for v in i['districtData'].values()])
-    deaths_list = list([v['deceased'] for i in r.values() for v in i['districtData'].values()])
-
-
-    r2 = requests.get('https://api.covid19india.org/data.json').json()
-    data = r2['statewise']
-
-    state_list = list(x['state'] for x in data)
-    confirmed_Slist = list(x['confirmed'] for x in data)
-    active_Slist = list(x['active'] for x in data)
-    recovered_Slist = list(x['recovered'] for x in data)
-    deaths_Slist = list(x['deaths'] for x in data)
-
-    #SQL COMMMANDS
-    try:
-        #DISTRICTS
-
-        #cursor.execute("DROP TABLE dis")
-        cursor.execute("CREATE TABLE dis(districts LONGTEXT, confirmed int, active int, recovered int, deaths int)")
-        query = "INSERT INTO dis (districts, confirmed, active, recovered, deaths) values (%s, %s, %s, %s, %s)"
-        cursor.executemany(query, [(a, b, c, d, e) for a, b, c, d, e in zip(districts, confirmed_list, active_list, recovered_list, deaths_list)])
-        mydb.commit()
-
-        #STATE
-
-        #cursor.execute("DROP TABLE state")
-        cursor.execute("CREATE TABLE state (states LONGTEXT, confirmed int,  active int, recovered int, deaths int)")
-        query = "INSERT INTO state (states, confirmed, active, recovered, deaths) VALUES (%s, %s, %s, %s, %s)"
-        cursor.executemany(query, [(a, b, c, d, e) for a, b, c, d, e in zip(state_list, confirmed_Slist, active_Slist, recovered_Slist, deaths_Slist)])
-        mydb.commit()
-
-        print("DB Exists, Refresh to get current values")
-    
-    except:
-        #DISTRICTS
-
-        cursor.execute("DROP TABLE dis")
-        cursor.execute("CREATE TABLE dis(districts LONGTEXT, confirmed int, active int, recovered int, deaths int)")
-        query = "INSERT INTO dis (districts, confirmed, active, recovered, deaths) values (%s, %s, %s, %s, %s)"
-        cursor.executemany(query, [(a, b, c, d, e) for a, b, c, d, e in zip(districts, confirmed_list, active_list, recovered_list, deaths_list)])
-        mydb.commit()
-
-        #STATE
-
-        cursor.execute("DROP TABLE state")
-        cursor.execute("CREATE TABLE state (states LONGTEXT, confirmed int,  active int, recovered int, deaths int)")
-        query = "INSERT INTO state (states, confirmed, active, recovered, deaths) VALUES (%s, %s, %s, %s, %s)"
-        cursor.executemany(query, [(a, b, c, d, e) for a, b, c, d, e in zip(state_list, confirmed_Slist, active_Slist, recovered_Slist, deaths_Slist)])
-        mydb.commit()
-
-        print("DB Created")
 
 def search(locality):
     global Gconfirmed
@@ -239,6 +171,5 @@ def countrySearch(locality):
 
 
 if __name__ == "__main__":
-    #createDB()
     main()
 
